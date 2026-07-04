043899ec11c728d000f3d88e11e93ba8fa956c19
iledgerlpg <iledgerlpg@gmail.com>
2026-07-04 14:51:48 +0700

    Update config.js

--- js/config.js
+++ js/config.js
@@ -7,5 +7,5 @@
 window.ILPG_CONFIG = {
   // Ganti dengan URL Web App hasil Deploy di Google Apps Script.
   // Cara dapatkan: GAS Script Editor → Deploy → Manage deployments → copy "Web app URL"
-  API_URL: 'https://script.google.com/macros/s/AKfycbyX6-0yA1SYfEfxwBzMplIBodnWqpg83koaY-dixnI18CdMbxasME4cY9xDTm7wA-kDsQ/exec',
+  API_URL: 'https://script.google.com/macros/s/AKfycbxuc3aLBSNQkPDIN3VvPpjfgSeMWct0GwkSt5ix6-TeXUQUPq7mXGN93mE8VtcA_QfOkA/exec',
 };
