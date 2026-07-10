4aeb6d938f951e82d50f035d4cec09c45acab65f
iledgerlpg <iledgerlpg@gmail.com>
2026-07-10 15:55:07 +0700

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,4 +1,4 @@
 window.ILPG_CONFIG = {
 
-  API_URL: 'https://script.google.com/macros/s/AKfycbwmCpNxs8HpaFXPbXrXQzeIZovUC8pCDQStfOJHNB3cIjDZXX0oSCRCrcvDyIIfOOsAUQ/exec',
+  API_URL: 'https://script.google.com/macros/s/AKfycbxTpk-mqE4nWPelS-X4sqpyIVxfNQfxAd6bmDjChTDXRviRiqlbgeASMTSTdwDTxrAWiA/exec',
 };
